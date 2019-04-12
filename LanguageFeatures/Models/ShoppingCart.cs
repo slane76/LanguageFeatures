8b0ffb3e4e396c1e0c191b7da143396fb7c58b38
Samuel Lane <samuel.lane.sa@outlook.com>
2019-04-12 09:25:53 -0400

    Add project files.

---
+++ LanguageFeatures/Models/ShoppingCart.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Collections;
+
+namespace LanguageFeatures.Models
+{
+    public class ShoppingCart : IEnumerable<Product>
+    {
+        public List<Product> Products { get; set; }
+
+        public IEnumerator<Product> GetEnumerator()
+        {
+            return Products.GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        public class ShoppingCart
+        {
+            public List<Product> Products { get; set; }
+        }
+    }
+}
